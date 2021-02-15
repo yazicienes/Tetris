382d558329a03ea8d256a4a0c7513e77babb16de
eyazici <eyazici@sfu.ca>
2021-02-14 17:31:23 -0800

    created main

--- main.py
+++ main.py
@@ -162,7 +162,7 @@ def check_lost(positions):
     pass
  
 def get_shape():
-    return random.choice(shapes)
+    return Piece(5, -2, random.choice(shapes))#piece at middle of the screen and above screen
  
  
 def draw_text_middle(text, size, color, surface):
@@ -197,7 +197,7 @@ def draw_window(surface, grid):
     draw_grid(surface, grid)
     pygame.display.update()
  
-def main():
+def main(win):
     locked_positions ={}
     grid = create_grid(locked_positions)
 
@@ -215,15 +215,27 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     current_piece.x -= 1
+                    if not (valid_space(current_piece, grid)):
+                        current_piece.x += 1
                 if event.key == pygame.K_RIGHT:
                     current_piece.x += 1
+                    if not (valid_space(current_piece, grid)):
+                        current_piece.x -= 1
                 if event.key == pygame.K_DOWN:
                     current_piece.y += 1
+                    if not (valid_space(current_piece, grid)):
+                        current_piece.y -= 1
                 if event.key == pygame.K_UP:
                     current_piece.rotation +=1
+                    if not (valid_space(current_piece, grid)):
+                        current_piece -= 1
+    draw_window(win, grid)
+                    
                 
  
-def main_menu():
-    pass
+def main_menu(win):
+    main(win)
  
-main_menu()
+win = pygame.display.set_mode((s_width, s_height))
+pygame.display.set_caption(('Tetris'))
+main_menu(win)
