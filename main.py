f6780a535a2ba3d84e3cebda568b4c34fb767992
eyazici <eyazici@sfu.ca>
2021-02-14 17:02:32 -0800

    implemented draw_grid

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ block_size = 30
 n_block = 10
 n_shapes = 7
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 
 # SHAPE FORMATS
  #2D represent rotation
@@ -157,14 +159,28 @@ def check_lost(positions):
     pass
  
 def get_shape():
-    pass
+    return random.choice(shapes)
  
  
 def draw_text_middle(text, size, color, surface):
     pass
    
 def draw_grid(surface, row, col):
-    pass
+    surface.fill(BLACK)
+    #title
+    pygame.font.init()
+    font = pygame.font.SysFont('comicsans', 60)
+    label = font.render('Tetris', 1, WHITE)
+    #draw label at the middle of the screen
+    surface.blit(label, (top_left_x, play_width/2-(label.get_width()/2), 30))
+
+    for i in range (len(grid)):
+        for j in range(len(grid[i])):
+            pygame.draw.rect(surface, grid[i][j], (top_left_x + j*block_size, top_left_y + i*block_size), 0)
+    
+    pygame.draw.rect(surface, RED, (top_left_x, top_left_y, play_width, play_height))
+
+    pygame.display.update()
  
 def clear_rows(grid, locked):
     pass
